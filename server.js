6cfda2c29260c05848ac08b252fed93c13e3b685
Randi Umphrey <33576756+randiU@users.noreply.github.com>
2026-03-07 22:26:27 -0700

    Finished connecting database. Currently in a working state.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import routes from "./src/controllers/routes.js";
+import { setupDatabase, testConnection } from "./src/models/setup.js";
 
 const app = express();
 
@@ -13,6 +14,18 @@ app.set("views", path.join(__dirname, "src/views"));
 
 app.use("/", routes);
 
-app.listen(3000);
+async function startServer() {
+  try {
+    await testConnection();   // tests PostgreSQL connection
+    await setupDatabase();    // runs seed.sql if needed
 
-console.log('Server is running on http://localhost:3000');
+    app.listen(3000, () => {
+      console.log("Server running on http://localhost:3000");
+    });
+
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+}
+
+startServer();
